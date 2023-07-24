c588d60cc8b5dd5b8c3fa85ee872bb800a68da8b
Enrique Pinedo <epinedo31@outlook.com>
2023-07-24 16:48:44 -0500

    window.confirm

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //Arrays for the characters needed in the password
-var uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowercase="abcdefghijklmnopqrstuvwxyz";
+var upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase="abcdefghijklmnopqrstuvwxyz";
 var numbers="0123456789";
-var special="~`!@#$%^&*()-_=+[]{}|;\"':<>,./?";
+var specialCharacters="~`!@#$%^&*()-_=+[]{}|;\"':<>,./?";
 
-// Assignment Code
+// "Generate Password" Button
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -18,30 +18,30 @@ function writePassword() {
 // Created a function called generatePassword
 function generatePassword(){
   var length = prompt("Enter the amount of characters you want in your Password.");
-  console.log(isNaN(length))
   if( isNaN(length) === true || length < 8 || length > 128){
     alert("wrong")
     generatePassword()
   }
 
+  // alert & window.confirm
+  var includeslowercase= confirm("Do you want lowercase letters in your password?");
+  var includesuppercase= confirm("Do you want uppercase letters in your password?");
+  var includesnumbers= confirm("Do you want numbers in your password?");
+  var includesspecialcharacters= confirm("Do you want special characters in your password?");
   
-  // return length;
-
-  // NaN
-  // alert & window.confirmed
-  // pool for what if & "or"= 'this or that'
+  // pool for what if 
   var pool = "";
   if (includeslowercase === true){
-    pool += lowercase;
+    pool += lowerCase;
   }
   if (includesuppercase === true){
-    pool += uppercase;
+    pool += upperCase;
   }
   if (includesnumbers === true){
     pool += numbers;
   }
   if (includesspecialcharacters === true){
-    pool += special;
+    pool += specialCharacters;
   }
 
   // math.random-for loop
